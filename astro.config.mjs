34c38604095a6a748eec559d0de2ca666d70a740
Mariano Gimenez <mariano@live.com>
2026-02-09 17:06:02 -0300

    add wrangler.jsonc

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+
+import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
-  site: 'https://www.guidoarlettaz.com',
-  integrations: [
-    tailwind(),
-    sitemap(),
-  ],
+  site: "https://guidoarlettaz.com.ar",
+
+  integrations: [tailwind(), sitemap()],
+
+  adapter: cloudflare(),
 });
